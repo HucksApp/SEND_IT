37f4717443a519a310f6b68be15dc9117334f950
HuckApps <jakehunt000000@gmail.com>
2020-01-22 02:37:21 -0800

    m

--- config/headers.js
+++ config/headers.js
@@ -1,9 +1,7 @@
 export  const header =  {
-                            "Content-Type":"application/json",
                             "x-timestamp":   Date.now(),
                             "Last-Modified": "Sun, 19 Jan 2016 04:32:39 GMT",
                             "Authorization":"JWT",
-                            "Access-Control-Max-Age": "3600",
                             "Host":"https://s-i-api.herokuapp.com/api/v1"
 
                         };
