acee4d395a529f9e9de2602656c529af0bb56571
HuckApps <jakehunt000000@gmail.com>
2020-01-22 03:08:49 -0800

    m

--- config/headers.js
+++ config/headers.js
@@ -1,5 +1,7 @@
 export  const header =  {
                             "x-timestamp":   Date.now(),
-                            "Last-Modified": "Sun, 19 Jan 2016 04:32:39 GMT"
+                            "Last-Modified": "Sun, 19 Jan 2016 04:32:39 GMT",
+                            "Authorization":"JWT",
+                            "Host":"https://s-i-api.herokuapp.com/api/v1"
 
                         };
