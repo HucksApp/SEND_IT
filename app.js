a75b1d338776bbc115c21d19b48f69c9fecf96f1
HuckApps <jakehunt000000@gmail.com>
2020-01-22 00:50:58 -0800

    m

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.use(jsonParser);
 app.use(function(req, res, next){
 
     res.set(header);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 
 });
