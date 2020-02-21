0fc31533454c3452c9bddfe2b15d7f0ee717c54b
HuckApps <jakehunt000000@gmail.com>
2020-02-21 02:58:03 -0800

    changes

--- src/components/CreateOrder.jsx
+++ src/components/CreateOrder.jsx
@@ -32,7 +32,6 @@ export class CreateOrder extends Component {
 
   handleCreate = (e) => {
     e.preventDefault();
-
     this.props.updateOrder(this.state.data.order);
 
     const newCopy = { ...this.state.data }
@@ -52,8 +51,8 @@ export class CreateOrder extends Component {
     return (
       <div className={'new_order ' + this.props.show} >
         <form className="create-order" onSubmit={this.handleCreate} >
+        <h3>NEW ORDER PLACEMENT</h3>
           <div>
-            <h3>NEW ORDER PLACEMENT</h3>
             <label htmlFor="receiverName">RECEIVERS NAME<sup style={{color:'#f00'}}>*</sup>:</label>
             <input id="receiverName" type="text" onChange={this.handleChange} value={this.state.data.order.receiverName} placeholder="ENTER THE RECEIVERS NAME" required />
           </div>
@@ -70,7 +69,7 @@ export class CreateOrder extends Component {
             <input id="description" type="text" onChange={this.handleChange} value={this.state.data.order.description} placeholder="PLEASE ENTER DESCRIPTION IN.. WEIGHT,CONTENT,TYPE" required />
           </div>
           <div>
-            <label htmlFor="receiverPhoneNumber" >RECEIVER PHONE NUMBER<sup style={{color:'#f00'}}>*</sup>:</label>
+            <label htmlFor="receiverPhoneNumber" >RECEIVER PHONE<sup style={{color:'#f00'}}>*</sup>:</label>
             <input id="receiverPhoneNumber" type="tel" onChange={this.handleChange} value={this.state.data.order.receiverPhoneNumber} placeholder="ENTER THE RECEIVER PHONE NO IN FORMAT 2349012345678" pattern="[2-4]{3}[0-9]{10}" required />
           </div>
           <button type="submit" title="SUBMIT">CREATE</button>
