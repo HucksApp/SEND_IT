2937107495d736e45981d1b61993711e0a0e7ac4
HuckApps <jakehunt000000@gmail.com>
2020-02-21 01:21:40 -0800

    changes

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -3,37 +3,37 @@ import React, { Component } from 'react';
 class Blog extends Component {
     state = {
         blogs: [
-            `"Even when you are marketing to your entire audience or customer base,
-             you are still simply speaking to a single human at any given time."Ann Handley`,
+            `Even when you are marketing to your entire audience or customer base,
+             you are still simply speaking to a single human at any given time..Ann Handley`,
 
             `Search marketing, and most Internet marketing in fact, can be very threatening because there are no rules.
              There’s no safe haven. To do it right, you need to be willing to be wrong.
-             But search marketing done right is all about being wrong. Experimentation is the only way."  Mike Moran`,
+             But search marketing done right is all about being wrong. Experimentation is the only way..Mike Moran`,
 
-            ` " Always deliver more than expected." — Larry Page, co-founder of Google `,
+            `  Always deliver more than expected... Larry Page, co-founder of Google `,
 
-            `"What do you need to start a business? Three simple things: know your product better than anyone,
-             know your customer, and have a burning desire to succeed."  Dave Thomas, Founder of Wendy’s`,
+            `What do you need to start a business? Three simple things: know your product better than anyone,
+             know your customer, and have a burning desire to succeed...Dave Thomas, Founder of Wendy’s`,
 
             `If you are not embarrassed by the first version of your product,
-              you’ve launched too late.”  Reid Hoffman, co-founder of LinkedIn`,
+              you’ve launched too late...Reid Hoffman, co-founder of LinkedIn`,
 
-            `“The stars will never align, and the traffic lights of life will never all be green at the same time.
+            `The stars will never align, and the traffic lights of life will never all be green at the same time.
                The universe doesn’t conspire against you, but it doesn’t go out of its way to line up the pins either. 
                Conditions are never perfect. ‘Someday’ is a disease that will take your dreams to the grave with you. 
                Pro and con lists are just as bad. If it’s important to you and you want to do it ‘eventually,’ 
-               just do it and correct course along the way.”  Tim Ferriss, author of The 4-Hour Work Week`,
+               just do it and correct course along the way...Tim Ferriss, author of The 4-Hour Work Week`,
 
-            `“Your work is going to fill a large part of your life,
+            `Your work is going to fill a large part of your life,
                 and the only way to be truly satisfied is to do what you believe is great work.
-                 And the only way to do great work is to love what you do.” Steve Jobs, co-founder, 
+                 And the only way to do great work is to love what you do... Steve Jobs, co-founder, 
                  Chairman and CEO of Apple Inc.`,
 
-            `“Always look for the fool in the deal. 
-                 If you don’t find one, it’s you.”  Mark Cuban, 
+            `Always look for the fool in the deal. 
+                 If you don’t find one, it’s you... Mark Cuban, 
                  AXS TV Chairman and entrepreneur `,
 
-            `“Your most unhappy customers are your greatest source of learning.” 
+            `Your most unhappy customers are your greatest source of learning...
                   Bill Gates, co-founder of Microsoft`,
 
             "Open Every Working Days, To Answer Your Call, We Are Trilled To hear You "
