5c89d728826efaffe430ea6f82fa57ee8e4c9fd6
HuckApps <jakehunt000000@gmail.com>
2020-02-28 16:24:06 -0800

    auto address

--- config/headers.js
+++
@@ -1,7 +0,0 @@
-export const header = {
-    "x-timestamp": Date.now(),
-    "Last-Modified": "Sun, 19 Jan 2016 04:32:39 GMT",
-    "Authorization": "JWT",
-    "Host": "https://s-i-api.herokuapp.com/api/v1"
-
-};
